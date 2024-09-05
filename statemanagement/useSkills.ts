c8f3e8e585f5dfca2c3d7373c79cab42bb82fbb8
sheraz4196 <sheraztoor321@gmail.com>
2024-09-05 18:00:22 +0500

    fix: skills stored

--- statemanagement/useSkills.ts
+++ statemanagement/useSkills.ts
@@ -1,43 +1,5 @@
-// import { create } from "zustand";
-
-// type SkillsEntryTypes = {
-//   skillsTitle: string | any;
-//   skillsLevel: string | any;
-// };
-
-// type SkillsHistoryTypes = {
-//   skillsHistory: { [key: number]: SkillsEntryTypes };
-//   setSkillsTitle: (index: number, skillsTitle: string) => void;
-//   setSkillsLevel: (index: number, skillsLevel: string) => void;
-// };
-
-// export const useSkills = create<SkillsHistoryTypes>((set) => ({
-//   skillsHistory: {},
-
-//   setSkillsTitle: (index, skillsTitle) =>
-//     set((state) => ({
-//       skillsHistory: {
-//         ...state.skillsHistory,
-//         [index]: {
-//           ...state.skillsHistory[index],
-//           skillsTitle,
-//         },
-//       },
-//     })),
-
-//   setSkillsLevel: (index, skillsLevel) =>
-//     set((state) => ({
-//       skillsHistory: {
-//         ...state.skillsHistory,
-//         [index]: {
-//           ...state.skillsHistory[index],
-//           skillsLevel,
-//         },
-//       },
-//     })),
-// }));
-
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 type SkillsEntryTypes = {
   skillsTitle: string | any;
@@ -51,42 +13,47 @@ type SkillsHistoryTypes = {
   setSkillsLevel: (index: number, skillsLevel: string) => void;
 };
 
-export const useSkills = create<SkillsHistoryTypes>((set) => ({
-  skillsHistory: {},
+export const useSkills = create<SkillsHistoryTypes>()(
+  persist(
+    (set) => ({
+      skillsHistory: {},
 
-  addSkill: (label) =>
-    set((state) => {
-      const nextIndex = Object.keys(state.skillsHistory).length;
-      return {
-        skillsHistory: {
-          ...state.skillsHistory,
-          [nextIndex]: {
-            skillsTitle: label,
-            skillsLevel: "60", // Default skill level
+      addSkill: (label) =>
+        set((state) => {
+          const nextIndex = Object.keys(state.skillsHistory).length;
+          return {
+            skillsHistory: {
+              ...state.skillsHistory,
+              [nextIndex]: {
+                skillsTitle: label,
+                skillsLevel: "60", // Default skill level
+              },
+            },
+          };
+        }),
+
+      setSkillsTitle: (index, skillsTitle) =>
+        set((state) => ({
+          skillsHistory: {
+            ...state.skillsHistory,
+            [index]: {
+              ...state.skillsHistory[index],
+              skillsTitle,
+            },
           },
-        },
-      };
+        })),
+
+      setSkillsLevel: (index, skillsLevel) =>
+        set((state) => ({
+          skillsHistory: {
+            ...state.skillsHistory,
+            [index]: {
+              ...state.skillsHistory[index],
+              skillsLevel,
+            },
+          },
+        })),
     }),
-
-  setSkillsTitle: (index, skillsTitle) =>
-    set((state) => ({
-      skillsHistory: {
-        ...state.skillsHistory,
-        [index]: {
-          ...state.skillsHistory[index],
-          skillsTitle,
-        },
-      },
-    })),
-
-  setSkillsLevel: (index, skillsLevel) =>
-    set((state) => ({
-      skillsHistory: {
-        ...state.skillsHistory,
-        [index]: {
-          ...state.skillsHistory[index],
-          skillsLevel,
-        },
-      },
-    })),
-}));
+    { name: "skills", storage: createJSONStorage(() => localStorage) }
+  )
+);
